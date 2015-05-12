94ee36cc8b0de42319972c08ea07fe971030fd84
Maxi Perez <maxi_perez3@hotmail.com>
2015-05-12 16:04:33 -0300

    Gente, en la DB, en la tabla Reservas, cambiar el tipo de dato del campo FECHA, lo tiene en INT ponerlo en DATE.

--- models/Reservas.php
+++ models/Reservas.php
@@ -33,7 +33,8 @@ class Reservas extends \yii\db\ActiveRecord
     {
         return [
             [['Fecha', 'idMenu', 'idEmpleado'], 'required'],
-            [['Fecha', 'idMenu', 'idEmpleado', 'Retiro'], 'integer']
+        	[['Fecha'], 'safe'],
+        	[['idMenu', 'idEmpleado', 'Retiro'], 'integer']
         ];
     }
 
